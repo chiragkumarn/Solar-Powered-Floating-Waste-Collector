64ff51d8f74b7ed13273f214af4e4411a4cc3b37
chiragkumarn <136550952+chiragkumarn@users.noreply.github.com>
2024-06-07 14:47:47 +0530

    img.py

---
+++ img.py
@@ -0,0 +1,24 @@
+# This python code will capture the images u want to train and store in the path u have specified in the line 18 
+import cv2    
+import time
+cpt = 0
+maxFrames = 30 # number of images (can be changes to any number).
+
+count=0
+cap=cv2.VideoCapture(0)
+while cpt < maxFrames:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    count += 1
+    if count % 3 != 0:
+        continue
+    frame=cv2.resize(frame,(640,480))
+    cv2.imshow("test window", frame) # show image in window
+    cv2.imwrite("/home/pi/Downloads/yolov8-custom-object-detection-googlecoralusb-main/images/arduino_uno_%d.jpg" %cpt, frame)##this is the path 
+    time.sleep(0.01)
+    cpt += 1
+    if cv2.waitKey(5)&0xFF==27:
+        break
+cap.release()   
+cv2.destroyAllWindows()
